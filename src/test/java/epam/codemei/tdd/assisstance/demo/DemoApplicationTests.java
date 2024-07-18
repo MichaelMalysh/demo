f89e6cf1a249552fb53f18ee83e49da338f024be
mykhailo_malysh <mykhailo_malysh@epam.com>
2024-07-18 12:45:40 +0300

    Initial commit

---
+++ src/test/java/epam/codemei/tdd/assisstance/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.codemei.tdd.assisstance.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
